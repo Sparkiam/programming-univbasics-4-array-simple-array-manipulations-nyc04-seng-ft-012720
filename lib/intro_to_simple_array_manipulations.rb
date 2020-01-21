01a84c7386987a276ca205606633cfd1ec4603d3
Dell Wilson <warren.wilsonjr89@gmail.com>
2020-01-21 18:13:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,7 +2,7 @@
     next_color = "violet"
     
     def using_push(array,next_color)
-      puts colors_in_the_rainbow.push(next_color)
+      array.push(next_color)
     end 
     
     # updated_array = using_push(colors_in_the_rainbow, next_color)
