ec362780209ecfe498d192b0de34e6b7ae4fd335
Dell Wilson <warren.wilsonjr89@gmail.com>
2020-01-21 17:50:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,5 @@
  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
     next_color = "violet"
     
+    using_push = colors_in_the_rainbow.push(next_color)
     updated_array = using_push(colors_in_the_rainbow, next_color)
